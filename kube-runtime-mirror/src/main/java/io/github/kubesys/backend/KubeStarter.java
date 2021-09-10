a8bf17009d5080c4d9a9ea0a50213570b0ab6a5a
Heng WU <wuheng@iscas.ac.cn>
2021-09-10 09:23:37 +0800

    support topic

--- kube-runtime-mirror/src/main/java/io/github/kubesys/backend/KubeStarter.java
+++ kube-runtime-mirror/src/main/java/io/github/kubesys/backend/KubeStarter.java
@@ -32,8 +32,8 @@ public class KubeStarter {
 		
 		
 		KubeMirror kubeMirror = new KubeMirror(kubeClient);
-		kubeMirror.start(new String[] {"doslab.io.Frontend", "Pod"});
-		
+//		kubeMirror.start(new String[] {"doslab.io.Frontend", "Pod"});
+		kubeMirror.start();
 	}
 	
 }
